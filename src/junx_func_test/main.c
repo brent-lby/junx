0d797569f1308123f1bcacccdbecfd541139d6e1
picofox <17509294@qq.com>
2024-08-18 22:56:08 +0800

    implement a windows version of dir enumeration.

--- src/junx_func_test/main.c
+++ src/junx_func_test/main.c
@@ -1,68 +1,114 @@
 ﻿#include <stdio.h>
 #include "junx/core/container/junx_vector.h"
 #include "junx/core/rtti/junx_object.h"
+#ifdef WINDOWS
+#else
 #include <dirent.h>
+#endif
 #include <string.h>
 #include <stdlib.h>
 
-void process_junx_objct(junx_object *  obj)
+#ifdef WINDOWS
+#define JUNX_PATH_MAX      MAX_PATH
+
+#else
+#define JUNX_PATH_MAX      PATH_MAX
+
+#endif
+
+#ifdef WINDOWS
+int print_dir_content(const char* dir_name, int level)
 {
+    HANDLE dp = INVALID_HANDLE_VALUE;
+    WIN32_FIND_DATA entry = {0};
+    BOOL b_success = TRUE;
 
+    char buffer[JUNX_PATH_MAX] = { 0 };
+    char leading_space[256] = { 0 };
+    int i = 0;
+    for (; i < level; i++)
+    {
+        if (i == 0)
+            strncat_s(leading_space, sizeof(leading_space), "|____", 5);
+        else
+            strncat_s(leading_space, sizeof(leading_space), "_____", 5);
+    }
 
-     printf("obj is %u\n", JUNX_METHODS(obj)->_type_id());
+    sprintf_s(buffer, sizeof(buffer), "%s\\*.*", dir_name);
+
+    dp = FindFirstFile(buffer, &entry);
+    if (dp == INVALID_HANDLE_VALUE)
+    {
+        return -1;
+    }
+    do
+    {
+        if ((entry.dwFileAttributes  &FILE_ATTRIBUTE_DIRECTORY) != 0)
+        {
+            printf("%s%s\t(dir)\n", leading_space, entry.cFileName);
+        }
+        else 
+        {
+            printf("%s%s\t(file)\n", leading_space, entry.cFileName);
+        }
+        b_success = FindNextFile(dp, &entry);
+    } while (FALSE != b_success);
     
+    FindClose(dp);
+    return 0;
 }
 
+#else
 int print_dir_content(const char* dir_name, int level)
 {
     int res = 0;
-    char buffer[PATH_MAX] = {0};
-    char leading_space[256] = {0};
+    char buffer[JUNX_PATH_MAX] = { 0 };
+    char leading_space[256] = { 0 };
     int i = 0;
-    for(;i < level; i++)
+    for (; i < level; i++)
     {
         if (i == 0)
             strncat(leading_space, "|____", 5);
         else
             strncat(leading_space, "_____", 5);
     }
-    DIR *dp = NULL;
-    struct dirent *entry_ptr = NULL;
+    DIR* dp = NULL;
+    struct dirent* entry_ptr = NULL;
 
     dp = opendir(dir_name);
-    if(dp == NULL)
+    if (dp == NULL)
     {
         return -1;
     }
-    while(1)
+    while (1)
     {
         entry_ptr = readdir(dp);
-        if(entry_ptr == NULL)
+        if (entry_ptr == NULL)
         {
             break;
         }
         else
         {
-            if(entry_ptr->d_type == DT_REG)
+            if (entry_ptr->d_type == DT_REG)
             {
-                printf("%s%s\t(file)\n",leading_space, entry_ptr->d_name);
+                printf("%s%s\t(file)\n", leading_space, entry_ptr->d_name);
             }
-            else if(entry_ptr->d_type == DT_DIR)
+            else if (entry_ptr->d_type == DT_DIR)
             {
-                if((strcmp(entry_ptr->d_name, "..") != 0)
-                  &&(strcmp(entry_ptr->d_name, ".") != 0)
+                if ((strcmp(entry_ptr->d_name, "..") != 0)
+                    && (strcmp(entry_ptr->d_name, ".") != 0)
 
-                )
+                    )
                 {
-                    printf("%s%s\t(dir)\n",leading_space, entry_ptr->d_name);
+                    printf("%s%s\t(dir)\n", leading_space, entry_ptr->d_name);
                     strncat(buffer, dir_name, strlen(dir_name));
                     strncat(buffer, "/", 1);
                     strncat(buffer, entry_ptr->d_name, strlen(entry_ptr->d_name));
                     res = print_dir_content(buffer, level + 1);
-                    if(res != 0)
+                    if (res != 0)
                     {
                         printf("failed to print dir: %s\n", entry_ptr->d_name);
-                        closedir(dp) ;;
+                        closedir(dp);;
                         exit(-1);
                     }
                 }
@@ -73,9 +119,21 @@ int print_dir_content(const char* dir_name, int level)
             }
         }
     }
-    closedir(dp) ;
+    closedir(dp);
     return 0;
 }
+
+
+#endif
+
+void process_junx_objct(junx_object *  obj)
+{
+
+
+     printf("obj is %u\n", JUNX_METHODS(obj)->_type_id());
+    
+}
+
 
 int main(int argc, char* argv[]) 
 {
